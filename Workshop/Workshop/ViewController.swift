30e2a520478286b231a12df718b042efd9ade742
Bea <totanes.beatrice@gmail.com>
2018-03-29 17:11:48 +0800

    Code refactor. Transferred the view extension to the extension.swift

--- Workshop/Workshop/ViewController.swift
+++ Workshop/Workshop/ViewController.swift
@@ -7,121 +7,136 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-    
-    let plusPhotoButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
-        return button
-    }  ()
-    
-    let emailTextField: UITextField = {
-        let tf = UITextField()
-        tf.placeholder = "Email"
-        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
-        tf.borderStyle = .roundedRect
-        tf.font = UIFont.systemFont(ofSize: 14)
-        return tf
-    }()
-    
-    let usernameTextField: UITextField = {
-        let tf = UITextField()
-        tf.placeholder = "Username"
-        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
-        tf.borderStyle = .roundedRect
-        tf.font = UIFont.systemFont(ofSize: 14)
-        return tf
-    }()
-    let passwordTextField: UITextField = {
-        let tf = UITextField()
-        tf.placeholder = "Password"
-        tf.isSecureTextEntry = true
-        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
-        tf.borderStyle = .roundedRect
-        tf.font = UIFont.systemFont(ofSize: 14)
-        return tf
-    }()
-
-    let signUpButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Sign Up", for: .normal)
-        button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
-       
-        
-        button.layer.cornerRadius = 5
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-        button.setTitleColor(.white, for: .normal)
-        return button
-    }()
-    
-    
-    override func viewDidLoad() {
-        view.backgroundColor = .white
-        super.viewDidLoad()
-        
-        view.addSubview(plusPhotoButton)
-        
-        plusPhotoButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 40, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 140, height: 140)
-       
- 
-        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-
-        
-        setupInputFields ()
-        
-        
-        }
-    fileprivate func setupInputFields() {
-        let redView = UIView ()
-        redView.backgroundColor = .red
-        
-        let stackView = UIStackView(arrangedSubviews:[emailTextField, usernameTextField, passwordTextField, signUpButton])
-        stackView.distribution = .fillEqually
-        stackView.axis = .vertical
-        stackView.spacing = 10
-        
-        view.addSubview(stackView)
-        
-        stackView.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 20, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 200)
-    }
-    
-    
-    }
-
-extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat,
-                width: CGFloat, height: CGFloat) {
-        
-        translatesAutoresizingMaskIntoConstraints = false
-        
-        if let top = top {
-        self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
-        }
-        
-        if let left = left {
-            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
-        }
-        
-        if let bottom = bottom {
-            bottom.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
-        }
-        
-        if let right = right {
-            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
-        }
-        
-        if width != 0 {
-            widthAnchor.constraint(equalToConstant: width).isActive = true
-            
-        }
-        
-        if height != 0 {
-            heightAnchor.constraint(equalToConstant: height).isActive = true
-        }
-        
-    }
+	
+	let plusPhotoButton: UIButton = {
+		let button = UIButton(type: .system)
+		
+		button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+		
+		return button
+	}  ()
+	
+	let emailTextField: UITextField = {
+		let textField = UITextField()
+		
+		textField.placeholder = "Email"
+		textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+		textField.borderStyle = .roundedRect
+		textField.font = UIFont.systemFont(ofSize: 14)
+		
+		return textField
+	}()
+	
+	let usernameTextField: UITextField = {
+		let tf = UITextField()
+		
+		tf.placeholder = "Username"
+		tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+		tf.borderStyle = .roundedRect
+		tf.font = UIFont.systemFont(ofSize: 14)
+		
+		return tf
+	}()
+	let passwordTextField: UITextField = {
+		let tf = UITextField()
+		
+		tf.placeholder = "Password"
+		tf.isSecureTextEntry = true
+		tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+		tf.borderStyle = .roundedRect
+		tf.font = UIFont.systemFont(ofSize: 14)
+		
+		return tf
+	}()
+	
+	let signUpButton: UIButton = {
+		let button = UIButton(type: .system)
+		
+		button.setTitle("Sign Up", for: .normal)
+		button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+		button.layer.cornerRadius = 5
+		button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+		button.setTitleColor(.white, for: .normal)
+		button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+		
+		return button
+		
+	}()
+	
+	@objc func handleSignUp (){
+		let email = "dummy0@gmail.com"
+		let password = "123123"
+		
+		Auth.auth().createUser(withEmail: email, password: password, completion: { (user:User?, error: Error?) in
+			
+			if let err = error {
+				print ("Failed to create user:", err)
+				return
+				
+			}
+			print("Successfully created user:", user?.uid ?? "")
+		})
+		
+	}
+	
+	override func viewDidLoad() {
+		view.backgroundColor = .white
+		super.viewDidLoad()
+		
+		view.addSubview(plusPhotoButton)
+		
+		setupInputFields ()
+		setupView()
+	}
+	
+	private func setupView() {
+		plusPhotoButton.anchor(top: view.topAnchor,
+													 left: nil,
+													 bottom: nil,
+													 right: nil,
+													 paddingTop: 40,
+													 paddingLeft: 0,
+													 paddingBottom: 0,
+													 paddingRight: 0,
+													 width: 140,
+													 height: 140)
+		
+		plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+		
+	}
+	
+	private func setupInputFields() {
+		let redView = UIView ()
+		redView.backgroundColor = .red
+		
+		let stackView = UIStackView(arrangedSubviews:[emailTextField,
+																									usernameTextField,
+																									passwordTextField,
+																									signUpButton])
+		stackView.distribution = .fillEqually
+		stackView.axis = .vertical
+		stackView.spacing = 10
+		
+		view.addSubview(stackView)
+		
+		stackView.anchor(top: plusPhotoButton.bottomAnchor,
+										 left: view.leftAnchor,
+										 bottom: nil,
+										 right: view.rightAnchor,
+										 paddingTop: 20,
+										 paddingLeft: 40,
+										 paddingBottom: 0,
+										 paddingRight: 40,
+										 width: 0,
+										 height: 200)
+	}
+	
 }
+
+
 
 
 
