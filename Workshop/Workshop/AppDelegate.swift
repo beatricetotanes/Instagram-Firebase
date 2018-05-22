3f355bc53e7223ef9c76758cda654f8527637bad
Beatrice Totanes <totanes.beatrice@gmail.com>
2018-05-22 23:39:05 +0800

    [🐛] Included makeKeyAndVisible

--- Workshop/Workshop/AppDelegate.swift
+++ Workshop/Workshop/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		
         FirebaseApp.configure()
         window = UIWindow()
+        window?.makeKeyAndVisible()
         window?.rootViewController = MainTabBarController()
         
         return true
